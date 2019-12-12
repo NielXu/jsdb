40a193905c4184852fc4709b41d13d6b29357ff6
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-12-12 10:41:44 -0500

    Added basic tests

--- test/db.test.js
+++ test/db.test.js
@@ -1,5 +1,36 @@
 const { BasicDatabase } = require('../src/db');
+const { expect } = require('chai');
 
-const basicA = new BasicDatabase([{key: "A", value: "B"}]);
-basicA.insert({key: "C", value: "D"});
-console.log(basicA.data);
+describe('BasicDatabase', () => {
+    
+    describe('Constuctor', () => {
+        it('should return what I passed to the constructor before any operations', () => {
+            const init = [{
+                key: "A", value: "B"
+            }, {
+                key: "C", value: "D"
+            }];
+            const basic = new BasicDatabase(init);
+            expect(basic.data).to.deep.equal(init);
+        });
+
+        it('should have empty array if nothing is passed to the constructor', () => {
+            const basic = new BasicDatabase();
+            expect(basic.data).to.deep.equal([]);
+        });
+    });
+
+    describe('Mutable', () => {
+        it('data should changed if modifying the data passed in', () => {
+            const init = [{
+                key: "A", value: "B"
+            }, {
+                key: "C", value: "D"
+            }];
+            const basic = new BasicDatabase(init);
+            expect(basic.data).to.deep.equal(init);
+            init[0]['value'] = 'X';
+            expect(basic.data).to.deep.equal(init);
+        })
+    });
+});

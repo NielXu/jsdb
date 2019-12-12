ca1a0381139b4358b4fd6b646d53de06d8072ec1
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-12-12 10:16:18 -0500

    Initial commit

---
+++ src/db.js
@@ -0,0 +1,30 @@
+class Database {
+    constructor(init) {
+        this.dbData = init;
+    }
+
+    get data() {
+        return this.dbData;
+    }
+
+    set data(newData) {
+        return this.newData;
+    }
+
+    insert(query) {
+
+    }
+
+    read(query) {
+
+    }
+
+    update(query, update) {
+
+    }
+
+    delete(query) {
+        
+    }
+    
+}

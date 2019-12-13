da7a56b73b1701a03e733fd921860cc6f21ae392
Daniel Xu <daniel.xu@fleetcomplete.com>
2019-12-13 17:07:18 -0500

    Changed database so that it can have multiple tables

--- src/db.js
+++ src/db.js
@@ -28,8 +28,46 @@ function findCandidateIndex(data, found) {
  * the constructor.
  */
 class Database {
-    constructor(init) {
-        this.data = init? init : [];
+    constructor() {
+        this.data = {};
+        this.using = null;
+    }
+
+    /**
+     * Create a new table with given name.
+     * 
+     * @param {String} tableName Name of the new table
+     * @param {Object} init The initial data be passed in the table, optional
+     */
+    create(tableName, init) {
+        if(this.data.hasOwnProperty(tableName)) {
+            throw `Table with name "${tableName}" already exists`;
+        }
+        this.data[tableName] = init? init : [];
+    }
+
+    /**
+     * Use a table, if no table was selected before
+     * querying, exception will be raised.
+     * 
+     * @param {String} tableName Name of the table
+     */
+    use(tableName) {
+        if(!this.data.hasOwnProperty(tableName)) {
+            throw `Table with name "${tableName}" does not exists`;
+        }
+        this.using = tableName;
+    }
+
+    /**
+     * List all the table names, return a list of strings
+     */
+    list() {
+        let names = [];
+        for(var key in this.data) {
+            names.push(key);
+        }
+        return names;
     }
 
     /**
@@ -37,9 +75,11 @@ class Database {
      * database.
      * 
      * @param {Object} document New document
+     * @param {String} tableName Table to use, this will ignore the current selection
+     * of the database, it is not recommended to use
      */
-    insert(document) {
-        this.data.push(document);
+    insert(document, tableName) {
+        this.data[this._checkUse(tableName)].push(document);
     }
 
     /**
@@ -48,25 +88,72 @@ class Database {
      * object all the data will be returned.
      * 
      * @param {Object} query Query object
+     * @param {String} tableName Table to use, this will ignore the current selection
+     * of the database, it is not recommended to use
      */
-    read(query) {
-        return parseQuery(this.data, query);
+    read(query, tableName) {
+        return parseQuery(this.data[this._checkUse(tableName)], query);
     }
 
-    update(query, update) {
-        const found = parseQuery(this.data, query);
-        const candidate = findCandidateIndex(this.data, found);
+    /**
+     * Update the data that matched the query. It the
+     * query has key like `{key.A.B: "someValue"}`, the
+     * update will not affect the existing fields, but if
+     * the query is `{key: {A: {B: "someValue"}}}`, the
+     * existing fields will be replaced.
+     * 
+     * @param {Object} query Query object
+     * @param {Object} update Update object
+     * @param {String} tableName Table to use, this will ignore the current selection
+     * of the database, it is not recommended to use
+     */
+    update(query, update, tableName) {
+        const data = this.data[this._checkUse(tableName)];
+        const found = parseQuery(data, query);
+        const candidate = findCandidateIndex(data, found);
         for(var i=0;i<candidate.length;i++) {
-            mergeDiff(this.data[candidate[i]], update, true);
+            mergeDiff(data[candidate[i]], update, true);
         }
     }
 
-    delete(query) {
-        const found = parseQuery(this.data, query);
-        const candidate = findCandidateIndex(this.data, found);
+    /**
+     * Delete all data that match the query, if the query
+     * is empty all data will be deleted.
+     * 
+     * @param {Object} query Query object
+     * @param {String} tableName Table to use, this will ignore the current selection
+     * of the database, it is not recommended to use
+     */
+    delete(query, tableName) {
+        const data = this.data[this._checkUse(tableName)];
+        const found = parseQuery(data, query);
+        const candidate = findCandidateIndex(data, found);
         for(var i=candidate.length-1;i>=0;i--) {
-            this.data.splice(candidate[i], 1);
+            data.splice(candidate[i], 1);
         }
+    }
+
+    /**
+     * Internal function, check the current use of table, return
+     * a correct table name or raise exception if error occured
+     * 
+     * @param {String} tableName Optional tableName
+     */
+    _checkUse(tableName) {
+        let table;
+        if(tableName) {
+            if(!this.data.hasOwnProperty(tableName)) {
+                throw `Table with name "${tableName}" does not exists`;
+            }
+            table = tableName;
+        }
+        else {
+            if(!this.using) {
+                throw "No table is selected";
+            }
+            table = this.using;
+        }
+        return table;
     }
 }
 
